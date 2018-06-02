0a267dc0761042748eb692d40df0748bc1f87957
Treinamento <treinamento@Treinamento1-Maquina14-2.local>
2018-06-02 09:21:47 -0300

    first commit

---
+++ SWFiveStars/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  SWFiveStars
+//
+//  Created by Treinamento on 12/05/2018.
+//  Copyright © 2018 Treinamento. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var name: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.button.backgroundColor = UIColor.white
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func save(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Usuario", in: context)
+        let newUser = NSManagedObject(entity: entity!, insertInto: context)
+
+        
+        newUser.setValue(name.text, forKey: "name")
+        do {
+            try context.save()
+        } catch {
+            print("Salvo com sucesso")
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Usuario")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "name") as! String)
+                
+            }
+            
+        } catch {
+            
+            print("Falhou")
+        }
+
+
+    }
+    
+    
+
+}
+
+        
+
+
---
+++ SWFiveStars/ViewControllerTabs.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerTabs.swift
+//  SWFiveStars
+//
+//  Created by Treinamento on 12/05/2018.
+//  Copyright © 2018 Treinamento. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTabs: UIViewController {
+    @IBOutlet weak var finalizar: UIButton!
+    
+    
+    override func viewDidLoad() {
+        
+       
+        super.viewDidLoad()
+        self.finalizar.backgroundColor = UIColor.white
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
